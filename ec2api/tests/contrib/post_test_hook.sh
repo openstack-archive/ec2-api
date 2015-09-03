8ea772867a05e42b83daafa995d41caa6b15683f
Andrey Pavlov <andrey-mp@yandex.ru>
2015-09-03 20:52:32 +0300

    Rework post test hook script
    Change-Id: I35a9916d843d81c70cd4e96492ed2d88bbe45bf8

--- ec2api/tests/contrib/post_test_hook.sh
+++ ec2api/tests/contrib/post_test_hook.sh
@@ -63,20 +63,28 @@ if [[ ! -f $TEST_CONFIG_DIR/$TEST_CONFIG ]]; then
   project_name="project-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8)"
   eval $(openstack project create -f shell -c id $project_name)
   project_id=$id
+  [[ -n "$project_id" ]] || (echo "Can't create project" && exit 1);
   user_name="user-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8)"
   eval $(openstack user create "$user_name" --project "$project_id" --password "password" --email "$user_name@example.com" -f shell -c id)
   user_id=$id
+  [[ -n "$user_id" ]] || (echo "Can't create user" && exit 1);
   # add 'Member' role for swift access
   role_id=$(openstack role show  Member -c id -f value)
   openstack role add --project $project_id --user $user_id $role_id
   # create network
   if [[ -n $(openstack service list | grep neutron) ]]; then
     net_id=$(neutron net-create --tenant-id $project_id "private" | grep ' id ' | awk '{print $4}')
+    [[ -n "$net_id" ]] || (echo "net-create failed" && exit 1);
     subnet_id=$(neutron subnet-create --tenant-id $project_id --ip_version 4 --gateway 10.0.0.1 --name "private_subnet" $net_id 10.0.0.0/24 | grep ' id ' | awk '{print $4}')
+    [[ -n "$subnet_id" ]] || (echo "subnet-create failed" && exit 1);
     router_id=$(neutron router-create --tenant-id $project_id "private_router" | grep ' id ' | awk '{print $4}')
+    [[ -n "$router_id" ]] || (echo "router-create failed" && exit 1);
     neutron router-interface-add $router_id $subnet_id
+    [[ "$?" -eq 0 ]] || (echo "router-interface-add failed" && exit 1);
     public_net_id=$(neutron net-list | grep public | awk '{print $2}')
+    [[ -n "$public_net_id" ]] || (echo "can't find public network" && exit 1);
     neutron router-gateway-set $router_id $public_net_id
+    [[ "$?" -eq 0 ]] || (echo "router-gateway-set failed" && exit 1);
   fi
   # populate credentials
   openstack ec2 credentials create --user $user_id --project $project_id 1>&2
@@ -94,47 +102,67 @@ if [[ ! -f $TEST_CONFIG_DIR/$TEST_CONFIG ]]; then
 
   # create EBS image
   MAX_FAIL=20
-  FLAVOR_NAME="m1.tiny"
+  FLAVOR_NAME="m1.ec2api"
   volume_status() { cinder show $1|awk '/ status / {print $4}'; }
   instance_status() { nova show $1|awk '/ status / {print $4}'; }
 
   openstack_image_id=$(openstack image list --long | grep "cirros" | grep " ami " | head -1 | awk '{print $2}')
-  volume_id=$(cinder create --image-id $openstack_image_id 1 | awk '/ id / {print $4}')
-  fail=0
-  until [[ $(volume_status $volume_id) == "available" ]]; do
-    if ((fail >= MAX_FAIL)); then
+  if [[ -n "$openstack_image_id" ]]; then
+    volume_id=$(cinder create --image-id $openstack_image_id 1 | awk '/ id / {print $4}')
+    [[ -n "$volume_id" ]] || (echo "can't create volume for EBS image creation" && exit 1);
+    fail=0
+    while [[ true ]] ; do
+      if ((fail >= MAX_FAIL)); then
+        echo "Volume creation fails (timeout)"
+        exit 1
+      fi
+      echo "attempt "$fail" of "$MAX_FAIL
+      status=$(volume_status $volume_id)
+      if [[ $status == "available" ]]; then
+        break
+      fi
+      if [[ $status == "error" || -z "$status" ]]; then
+        cinder show $volume_id
+        exit 1
+      fi
+      sleep 10
+      ((++fail))
+    done
+
+    instance_name="i-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8)"
+    instance_id=$(nova boot \
+      --flavor "$FLAVOR_NAME" \
+      --block-device "device=/dev/vda,id=$volume_id,shutdown=remove,source=volume,dest=volume,bootindex=0" \
+      "$instance_name" | awk '/ id / {print $4}')
+    [[ -n "$instance_id" ]] || (echo "can't boot EBS instance" && exit 1);
+    fail=0
+    while [[ true ]] ; do
+      if ((fail >= MAX_FAIL)); then
+        echo "Instance active status wait timeout occured"
+        exit 1
+      fi
+      echo "attempt "$fail" of "$MAX_FAIL
+      status=$(instance_status $instance_id)
+      if [[ "$status" == "ACTIVE" ]]; then
+        break
+      fi
+      if [[ "$status" == "ERROR" || -z "$status" ]]; then
+        nova show $instance_id
+        exit 1
+      fi
+      sleep 10
+      ((++fail))
+    done
+
+    image_name="image-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8)"
+    nova image-create $instance_name $image_name
+    if [[ "$?" -ne "0" ]]; then
+      echo "Image creation from instance fails"
       exit 1
     fi
-    ((++fail)); sleep 5
-    if [[ $(volume_status $volume_id) == error ]]; then
-      cinder show $volume_id
-    fi
-  done
-  instance_name="i-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8)"
-  instance_id=$(nova boot \
-    --flavor "$FLAVOR_NAME" \
-    --block-device "device=/dev/vda,id=$volume_id,shutdown=remove,source=volume,dest=volume,bootindex=0" \
-    "$instance_name" | awk '/ id / {print $4}')
-  fail=0
-  until [[ $(instance_status $instance_id) == "ACTIVE" ]]; do
-    if ((fail >= MAX_FAIL)); then
-      exit 1
-    fi
-    ((++fail))
-    sleep 10
-    if [[ $(instance_status $instance_id) == "ERROR" ]]; then
-      nova show $instance_id
-      exit 1
-    fi
-  done
-  image_name="image-$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8)"
-  nova image-create $instance_name $image_name
-  if [[ "$?" -ne "0" ]]; then
-    echo "Image creation from instance fails"
-    exit 1
+    ebs_image_id=$(euca-describe-images --show-empty-fields | grep $image_name | awk '{print $2}')
+    nova delete $instance_id
   fi
-  ebs_image_id=$(euca-describe-images --show-empty-fields | grep $image_name | awk '{print $2}')
-  nova delete $instance_id
 
   timeout="180"
   run_long_tests="False"
