62ae9997fbc934f6eae891b9ce12406fd6f304b6
Feodor Tersin <ftersin@hotmail.com>
2015-09-05 13:26:08 +0300

    Use keystone session for admin context
    Since that oslo_context is used in ec2api, admin context is cached
    between service calls. This leads to that context auth token becomes
    outdated, because it has no renewal logic. This is not a big problem for
    ec2api service, because each new initialization of regular context
    overwrites a previous cached context. But this is a problem for metadata
    service, where is no regular context used.
    
    Instead of disabling caching of admin contexts this patch uses the
    caching in a separate thread storage. To handle token outdating Keystone
    session is used.
    
    Change-Id: I714419dc193471dde8a15715cbaeec83896809c9

--- ec2api/tests/unit/test_clients.py
+++ ec2api/tests/unit/test_clients.py
@@ -33,14 +33,16 @@ class ClientsTestCase(base.BaseTestCase):
         context = mock.NonCallableMock(
             auth_token='fake_token',
             service_catalog=[{'type': 'compute',
-                              'endpoints': [{'publicURL': 'nova_url'}]}])
+                              'endpoints': [{'publicURL': 'nova_url'}]}],
+            session=None)
 
         # test normal flow with get_api_version call
         res = clients.nova(context)
         self.assertEqual(nova.return_value, res)
         nova.assert_called_with(
-            '2.3', bypass_url='nova_url', http_log_debug=False,
-            auth_url='keystone_url', auth_token='fake_token')
+            '2.3', bypass_url='nova_url', http_log_debug=False, session=None,
+            auth_url='keystone_url', auth_token='fake_token',
+            service_type='compute')
         get_api_version.assert_called_once_with(context)
 
         # test logging with debug option and no get_api_version call
@@ -48,8 +50,9 @@ class ClientsTestCase(base.BaseTestCase):
         self.configure(debug=True)
         clients.nova(context)
         nova.assert_called_with(
-            '2.3', bypass_url='nova_url', http_log_debug=True,
-            auth_url='keystone_url', auth_token='fake_token')
+            '2.3', bypass_url='nova_url', http_log_debug=True, session=None,
+            auth_url='keystone_url', auth_token='fake_token',
+            service_type='compute')
         self.assertFalse(get_api_version.called)
         self.configure(debug=False)
 
@@ -61,12 +64,14 @@ class ClientsTestCase(base.BaseTestCase):
         clients.nova(context)
         nova.assert_called_with(
             '2.3', bypass_url='nova_legacy_url', http_log_debug=False,
-            auth_url='keystone_url', auth_token='fake_token')
+            auth_url='keystone_url', auth_token='fake_token', session=None,
+            service_type='compute_legacy')
 
     @mock.patch.object(clients, '_url_for')
     @mock.patch('novaclient.client.Client')
     def test_get_api_version(self, nova, url_for):
-        context = mock.NonCallableMock(auth_token='fake_token')
+        context = mock.NonCallableMock(auth_token='fake_token',
+                                       session=None)
         v2 = mock.NonCallableMock()
         v2.configure_mock(id='v2',
                           version='',
@@ -84,8 +89,9 @@ class ClientsTestCase(base.BaseTestCase):
             res = clients._get_nova_api_version(context)
         self.assertEqual(clients.REQUIRED_NOVA_API_MICROVERSION, res)
         url_for.assert_called_with(context, service_type='compute')
-        nova.assert_called_with('2.1', http_log_debug=False,
-                                bypass_url=url_for.return_value)
+        nova.assert_called_with('2.1', http_log_debug=False, session=None,
+                                bypass_url=url_for.return_value,
+                                service_type='compute')
         nova.return_value.versions.get_current.assert_called_with()
         self.assertTrue(logs.output.startswith('[INFO]'))
 
@@ -94,8 +100,9 @@ class ClientsTestCase(base.BaseTestCase):
         self.configure(nova_service_type='compute')
         clients._get_nova_api_version(context)
         url_for.assert_called_with(context, service_type='compute')
-        nova.assert_called_with('2.1', http_log_debug=True,
-                                bypass_url=url_for.return_value)
+        nova.assert_called_with('2.1', http_log_debug=True, session=None,
+                                bypass_url=url_for.return_value,
+                                service_type='compute')
         self.configure(debug=False)
         self.configure(nova_service_type='compute')
 
@@ -140,11 +147,12 @@ class ClientsTestCase(base.BaseTestCase):
         context = mock.NonCallableMock(
             auth_token='fake_token',
             service_catalog=[{'type': 'network',
-                              'endpoints': [{'publicURL': 'neutron_url'}]}])
+                              'endpoints': [{'publicURL': 'neutron_url'}]}],
+            session=None)
         res = clients.neutron(context)
         self.assertEqual(neutron.return_value, res)
         neutron.assert_called_with(
-            auth_url='keystone_url', service_type='network',
+            auth_url='keystone_url', service_type='network', session=None,
             token='fake_token', endpoint_url='neutron_url')
 
     @mock.patch('glanceclient.client.Client')
@@ -152,7 +160,8 @@ class ClientsTestCase(base.BaseTestCase):
         context = mock.NonCallableMock(
             auth_token='fake_token',
             service_catalog=[{'type': 'image',
-                              'endpoints': [{'publicURL': 'glance_url'}]}])
+                              'endpoints': [{'publicURL': 'glance_url'}]}],
+            session=None)
         res = clients.glance(context)
         self.assertEqual(glance.return_value, res)
         glance.assert_called_with(
@@ -165,12 +174,13 @@ class ClientsTestCase(base.BaseTestCase):
         context = mock.NonCallableMock(
             auth_token='fake_token',
             service_catalog=[{'type': 'volume',
-                              'endpoints': [{'publicURL': 'cinder_url'}]}])
+                              'endpoints': [{'publicURL': 'cinder_url'}]}],
+            session=None)
         res = clients.cinder(context)
         self.assertEqual(cinder.return_value, res)
         cinder.assert_called_with(
             '1', auth_url='keystone_url', service_type='volume',
-            username=None, api_key=None, http_log_debug=False)
+            username=None, api_key=None, http_log_debug=False, session=None)
         self.assertEqual('fake_token', res.client.auth_token)
         self.assertEqual('cinder_url', res.client.management_url)
 
@@ -179,7 +189,7 @@ class ClientsTestCase(base.BaseTestCase):
         clients.cinder(context)
         cinder.assert_called_with(
             '1', auth_url='keystone_url', service_type='volume',
-            username=None, api_key=None, http_log_debug=True)
+            username=None, api_key=None, http_log_debug=True, session=None)
 
     @mock.patch('ec2api.context.get_keystone_client_class',
                 return_value=mock.Mock(return_value=mock.Mock()))
